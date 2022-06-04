6c08c9b15794df604e06ab16526985e74fa16d08
yondav <yoni@yondav.us>
2022-06-04 03:07:26 -0400

    form elements

--- src/stories/Form/Form.stories.tsx
+++ src/stories/Form/Form.stories.tsx
@@ -7,13 +7,36 @@ export default {
   argTypes: {},
 };
 
+const styles = {
+  width: '400px',
+  height: '250px',
+};
+
 const InputTemplate: Story = props => (
-  <form>
-    <Form.Input {...props} />
-  </form>
+  <Form.Input {...props} tw='w-40' style={{ width: '400px' }} />
 );
 
-const TextAreaTemplate: Story = props => <Form.TextArea {...props} />;
+const TextAreaTemplate: Story = props => <Form.TextArea {...props} style={styles} />;
+
+const SelectTemplate: Story = props => (
+  <Form.Select {...props} style={{ width: '400px' }}>
+    <option>Option one</option>
+  </Form.Select>
+);
+
+const FieldSetTemplate: Story = props => (
+  <Form.FieldSet {...props} style={styles}>
+    <legend>Fieldset</legend>
+  </Form.FieldSet>
+);
+
+const SwitchTemplate: Story = props => <Form.Switch {...props} />;
+
+const CheckBoxTemplate: Story = props => <Form.CheckBox {...props} />;
 
 export const Input = InputTemplate.bind({});
 export const TextArea = TextAreaTemplate.bind({});
+export const Select = SelectTemplate.bind({});
+export const FieldSet = FieldSetTemplate.bind({});
+export const Switch = SwitchTemplate.bind({});
+export const CheckBox = CheckBoxTemplate.bind({});

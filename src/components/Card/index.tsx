a544a3a10e626120ea6b0333ee3b06d6aa9abfdc
yondav <yoni@yondav.us>
2022-06-15 02:54:39 -0400

    card gradient dirextion

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -4,6 +4,7 @@ import tw, { styled, css } from 'twin.macro';
 export interface CardProps {
   featured?: boolean;
   image?: boolean;
+  direction?: 'right' | 'bottom' | 'left' | 'top';
 }
 
 export interface BulletProps {
@@ -11,12 +12,12 @@ export interface BulletProps {
   complete?: boolean;
 }
 
-const CardBase = styled.div<CardProps>(({ featured }) => [
+const CardBase = styled.div<CardProps>(({ featured, direction = 'bottom' }) => [
   tw`w-full rounded-xl p-2.5 bg-neutral-900 transition-all duration-300 ease-linear`,
   featured &&
     css`
       background: linear-gradient(
-        var(--purple-700) 0%,
+        to ${direction} var(--purple-700) 0%,
         var(--pink-500) 33%,
         rgba(0, 0, 0, 0) 66%
       );

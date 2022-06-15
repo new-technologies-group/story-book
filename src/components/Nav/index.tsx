3f5f54eae12f0e13425f9d5a4c56c6569ad2fd99
yondav <yoni@yondav.us>
2022-06-15 00:32:14 -0400

    hamburger padding

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -16,7 +16,7 @@ export interface SideNavProps {
 }
 
 const Base = styled.nav<BaseNavProps>(({ sticky }) => [
-  tw`fixed top-0 left-0 max-h-20 py-2 px-4 w-screen font-primary text-base font-light text-white transition-all duration-700 ease-linear border-b border-neutral-700 flex justify-between items-center z-20`,
+  tw`fixed top-0 left-0 max-h-20 py-2 pl-4 md:px-4 w-screen font-primary text-base font-light text-white transition-all duration-700 ease-linear border-b border-neutral-700 flex justify-between items-center z-20`,
   sticky && tw`bg-neutral-900`,
 ]);
 

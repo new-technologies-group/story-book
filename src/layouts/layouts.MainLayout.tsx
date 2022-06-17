db7d6dfc160c9cb8e94c1eae52ff241a368dae31
yondav <yoni@yondav.us>
2022-06-17 10:47:58 -0400

    debugging export of main layout

--- src/layouts/layouts.MainLayout.tsx
+++ src/layouts/layouts.MainLayout.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
-import { Nav, Footer } from '../../components';
+import { Nav, Footer } from '../components';
 
 const MainLayout: React.FC<{ links: JSX.Element[] }> = ({ links }) => {
   return (

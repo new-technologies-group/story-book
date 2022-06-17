136c9eb4aef483971758901282bf4d1d7685422f
yondav <yoni@yondav.us>
2022-06-17 11:12:04 -0400

    modal context and layout

---
+++ src/context/context.modal.tsx
@@ -0,0 +1,42 @@
+import React, { createContext, useState, useEffect } from 'react';
+
+interface ModalContext {
+  isOpen: boolean;
+  toggleModal?: () => void;
+}
+
+const defaultState = {
+  isOpen: false,
+};
+
+export const ModalContext = createContext<ModalContext>(defaultState);
+
+export const ModalProvider: React.FC = ({ children }) => {
+  const [isOpen, setIsOpen] = useState(defaultState.isOpen);
+
+  const toggleModal = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const ROOT = document.getElementById('root');
+
+  useEffect(() => {
+    if (isOpen) {
+      (ROOT as HTMLDivElement).style.height = '100vh';
+      (ROOT as HTMLDivElement).style.overflowY = 'hidden';
+    } else {
+      (ROOT as HTMLDivElement).style.height = '';
+      (ROOT as HTMLDivElement).style.overflowY = '';
+    }
+  }, [isOpen]);
+
+  return (
+    <ModalContext.Provider value={{ isOpen, toggleModal }}>
+      {children}
+    </ModalContext.Provider>
+  );
+};
+
+export function useModal() {
+  return React.useContext(ModalContext);
+}

16761fe40911c010eac4eb433f8cb9dbfe23ebab
yondav <yoni@yondav.us>
2022-07-13 14:19:09 -0400

    corrected prop types on switch and checkbox components. style: size options

--- src/components/Form/Form.types.ts
+++ src/components/Form/Form.types.ts
@@ -1,3 +1,9 @@
-export interface FormElementProps {
+import { InputHTMLAttributes } from 'react';
+
+export interface FormElementProps extends InputHTMLAttributes<HTMLInputElement> {
   invalid?: boolean;
+}
+
+export interface CheckBoxProps extends InputHTMLAttributes<HTMLInputElement> {
+  lg?: boolean;
 }
